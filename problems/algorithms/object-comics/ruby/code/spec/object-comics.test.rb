0bf18569a9e508daf9c361dedbc0698481bd2655
neilprado <neiljohn17@gmail.com>
2019-09-21 17:36:36 -0300

    Object-Comics doing

---
+++ problems/algorithms/object-comics/ruby/code/spec/object-comics.test.rb
@@ -0,0 +1,57 @@
+require_relative "../src/object-comics"
+characters = [
+  {
+    comics: 'Marvel',
+    height: 1.78,
+    link: [
+      'http://marvel.com/characters/54/spider-man',
+      'http://marvel.com/universe/Spider-Man_(Peter_Parker)',
+    ],
+    name: 'Spider-Man',
+    realName: 'Peter Benjamin Parker',
+    weight: 76,
+  },
+  {
+    comics: 'Marvel',
+    height: 2.44,
+    link: [
+      'http://marvel.com/characters/25/hulk',
+      'http://marvel.com/universe/Hulk_(Bruce_Banner)',
+    ],
+    name: 'Hulk',
+    realName: 'Robert Bruce Banner',
+    weight: 635,
+  },
+  {
+    comics: 'DC',
+    height: 1.91,
+    link: [
+      'http://www.dccomics.com/characters/superman',
+      'http://dc.wikia.com/wiki/Superman_(Clark_Kent)',
+    ],
+    name: 'Superman',
+    realName: 'Clark Joseph Kent',
+    weight: 107,
+  },
+  {
+    comics: 'DC',
+    height: 1.88,
+    link: [
+      'http://www.dccomics.com/characters/batman',
+      'http://dc.wikia.com/wiki/Bruce_Wayne_(New_Earth)',
+    ],
+    name: 'batman',
+    realName: 'Bruce Wayne',
+    weight: 95,
+  }
+]
+
+describe Comics do
+  describe ".namesByComics" do
+    context "Listing names by DC comics" do
+      it "return ['batman', 'Superman']" do
+        expect(Comics.namesByComics(characters, 'DC')).to eql(['batman', 'Superman'])
+      end
+    end
+  end
+end

b90590d18093ab7a95010a281438017f231d2666
Luiz <lucachaves@gmail.com>
2023-03-23 09:37:28 -0300

    isolar exercícios em /pages/exercises

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-for f in src/pages/problems/*; do
-	exercise="${f/src\/pages\/problems\//}"
+rm -r "dist/exercises"
+
+for f in src/pages/exercises/*; do
+	exercise="${f/src\/pages\/exercises\//}"
 
   if ! [[ "$exercise" =~ ^_.*  ]]; then
-    originPath="src/pages/problems/${exercise}/_codes"
+    originPath="src/pages/exercises/${exercise}/_codes"
 
     if [ -d $originPath ]; then
       cp -r $originPath "dist/problems/${exercise}/_codes"
-
-      rm -r "dist/problems/${exercise}/README"
     fi
   fi
 done

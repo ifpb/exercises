2fdf762c9885eefa64cdf4cdea56f1c67c9d4cee
Luiz <lucachaves@gmail.com>
2024-03-22 09:47:46 -0300

    remover o jest.test.js

--- src/pages/exercises/test.sh
+++ src/pages/exercises/test.sh
@@ -1,5 +1,5 @@
-node --test **/js/response/*.node.test.js
-# node --test ./src/pages/exercises/array-*/_codes/js/response/*.node.test.js
+node --test **/js/response/*.test.js
+# node --test ./src/pages/exercises/array-*/_codes/js/response/*.test.js
 # find ./src/pages/exercises -type f -name '*.test.js' | grep -vE 'jest|node_modules|/code/' | grep array | xargs -I {} node --test {}
 # find . -type f -name '*.test.js' | grep -vE 'jest|node_modules|/code/' | grep array | xargs -I {} node --test {}
 # find ./src/pages/exercises -type f -name '*.test.js' |
